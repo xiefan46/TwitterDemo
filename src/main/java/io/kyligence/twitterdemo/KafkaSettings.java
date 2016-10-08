d4775c73ef95852d203d05f9f4a0bd637e0d0959
Yiming Liu <liuyiming.vip@gmail.com>
2016-10-08 15:17:54 +0800

    Init commit

---
+++ src/main/java/io/kyligence/twitterdemo/KafkaSettings.java
@@ -0,0 +1,25 @@
+package io.kyligence.twitterdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by liuyiming on 6/23/16.
+ */
+@Configuration
+public class KafkaSettings {
+    @Value("${kafka.brokers}")
+    private String brokers;
+
+    @Value("${kafka.topic}")
+    private String topic;
+
+    public String getBrokers() {
+        return brokers;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+}
